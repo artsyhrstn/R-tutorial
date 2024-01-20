86e6ad4ae620e7ff6fac45dbf369cc84e5fbc473
Hisbi <hisbiasyhrstn@gmail.com>
2024-01-21 03:15:34 +0700

    Introduction R

---
+++ Coba.r
@@ -0,0 +1,11 @@
+> print("Coding in R")
+[1] "Coding in R"
+> ?print()
+> # nama variabel harus dimulai dengan huruf dan dapat memuat nomor dan underscore
+> # Contoh variabel
+> first_variable <- "Variabel saya"
+> second_variable <- 12.5
+> vec_1 <- c(13, 48.5, 71, 101.5, 2)
+> vec_1
+[1]  13.0  48.5  71.0 101.5   2.0
+> # Pipes
