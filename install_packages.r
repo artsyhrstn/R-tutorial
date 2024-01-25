0874dda73178ea46743a2dd9af5892199a1932fa
Hisbi <hisbiasyhrstn@gmail.com>
2024-01-25 11:04:39 +0700

    Update

---
+++ install_packages.r
@@ -0,0 +1,5 @@
+# library membuaka file .dta ataupun file berformat tabel
+install.packages("Rcpp")
+
+# pengolahan data import dan export data, membuat visualisasi data, dan pemodelan data.
+install.packages("Rcpp")
