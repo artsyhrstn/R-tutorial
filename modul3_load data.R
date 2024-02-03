4b34c8d7d3a2ba3e8eb403265f93e0a092d9b34e
Hisbi <hisbiasyhrstn@gmail.com>
2024-02-03 15:34:22 +0700

    Update

--- modul3_load data.R
+++ modul3_load data.R
@@ -3,4 +3,18 @@ data()
 # Mencoba untuk memuat data mtcars (Motor Trend Car Road Tests)
 data(mtcars)
 
+#Install Package
+install.packages("Rcpp")
+library(readr)
+readr_example()
+read_csv(readr_example("mtcars.csv"))
+
+# Memuat file excel
 library(readxl)
+readxl_example()
+read_excel(readxl_example("type-me.xlsx"))
+
+excel_sheets(readxl_example("type-me.xlsx"))
+read_csv()
+
+read_excel(readxl_example("type-me.xlsx"), sheet = "numeric_coercion")
