60f3d79017b245e70ea132d3f175e54be9614467
Hisbi <hisbiasyhrstn@gmail.com>
2024-02-04 22:02:50 +0700

    Update

---
+++ modul3_operator lain untuk R.R
@@ -0,0 +1,42 @@
+# Modul 3 # Operator R lain
+
+# 4 Tipe
+# 1. Arithmetic
+# 2. Relational
+# 3. Logical
+# 4. Assignment
+
+# 1. ARITMATIK
+x <- 2
+y <- 5
+
+x + y # Penambahan
+x - y # Pengurangan
+x * y # Pengkalian
+x / y # Pembagian
+y %% x # Modulo
+y%/% x # Integer division
+y ^ x # Eksponen
+
+# 2. RELATIONAL
+x < y
+x > y
+x <= 2
+y >= 10
+y == 5
+x != 2
+
+# 3. LOGICAL OPERATORS
+& (Logical AND)
+&& (AND)
+| (OR)
+|| (Logical OR)
+! (Logical Not)
+
+# 4. Assignment
+x <- 4
+y <<- 7
+z = 9 
+a = 9
+11 -> x 
+21 ->> x # Scoping = scoping assignment is for advanced R users
