f0447257883b53c9be7dab706fdd987f5bf2965a
Hisbi <hisbiasyhrstn@gmail.com>
2024-02-09 07:41:15 +0700

    Update

--- modul3_Change Data.R
+++ modul3_Change Data.R
@@ -15,3 +15,18 @@ hotel_bookings <- read_csv("hotel_bookings.csv")
 head(hotel_bookings)
 str(hotel_bookings)
 colnames(hotel_bookings)
+
+hotel_bookings_city <- 
+  filter(hotel_bookings, hotel_bookings$hotel=="City Hotel")
+
+mean(hotel_bookings$lead_time)
+mean(hotel_bookings_city$lead_time)
+
+hotel_summary <- 
+  hotel_bookings %>%
+  group_by(hotel) %>%
+  summarise(average_lead_time=mean(lead_time),
+            min_lead_time=min(lead_time),
+            max_lead_time=max(lead_time))
+
+head(hotel_summary)
