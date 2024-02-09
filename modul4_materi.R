b9ec68325beb3b636a3731ccd041af0bada1fd9b
Hisbi <hisbiasyhrstn@gmail.com>
2024-02-09 07:57:37 +0700

    Update

---
+++ modul4_materi.R
@@ -0,0 +1,12 @@
+# Visualizing in R
+# Materi 4
+
+# Library Visualisasi
+# ggplot2, Plotly, Lattice, RGL, Dygraphs, Leaflet, Highcarter, Patchwork, gganimate, ggridges
+
+# GG(grammar graphics) plot 
+# + Create different types of plots
+# + Customize look
+# + High quality visual
+# Concepts = Aesthetics, Geoms, Facets, Labels and Annotation
+
