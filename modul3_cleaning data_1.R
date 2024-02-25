c1d57495fea62023a80f64a13d7de325aa0da765
Hisbi Asyihristani <hisbiasyhrstn@gmail.com>
2024-02-25 18:04:53 +0700

    Update

---
+++ modul3_cleaning data_1.R
@@ -0,0 +1,35 @@
+# Modul 3 - Cleaning Data
+install.packages("here")
+install.packages("skimr")
+install.packages("janitor")
+install.packages("dplyr")
+
+library("skimr") # Makes sumarizing data
+library("janitor") # Untuk melakukan cleaning data
+library("dplyr")
+
+read_csv() # Input file nama sendiri dalam kurung
+
+# Memuat dataset penguin
+install.packages("palmerpenguins")
+library(palmerpenguins)
+
+# Beberapa fungsi cleaning
+skim_without_charts(penguins)
+glimpse(penguins)
+head(penguins)
+
+# Menampilkan kolom tertentu
+penguins %>%
+  select(species)
+
+penguins %>%
+  select(-species) # selain kolom tertentu (-)
+
+# Mengganti nama variabel (kolom)
+penguins %>%
+  rename(island_new=island)
+
+rename_with(penguins,tolower) #toupper / tolower (janitor)
+
+clean_names(penguins) # memastikan hanya karakter, nomor, dan underscore yang berada pada nama kolom
