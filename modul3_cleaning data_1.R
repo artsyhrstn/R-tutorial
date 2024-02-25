e493e71299fafa73d14912616d5c0a9df11b5a4f
Hisbi Asyihristani <hisbiasyhrstn@gmail.com>
2024-02-25 22:10:09 +0700

    Update

--- modul3_cleaning data_1.R
+++ modul3_cleaning data_1.R
@@ -51,3 +51,37 @@ penguins %>% group_by(island, species) %>% drop_na() %>% summarize(max_bl = max(
 
 # 3. Filter
 penguins %>% filter (species == "Adelie") %>% drop_na() # Hanya menampilkan spesies Adelie
+
+
+# ------- TRANSFORMING DATA ----------
+
+# Membuat dataframe
+id <- c(1:10)
+name <- c("John Mendes", "Rob Stewart", "Rachel Abrahamson", "Christy Hickman", "Johnson Harper", "Candace Miller", "Carlson Landy", "Pansy Jordan", "Darius Berry", "Claudia Garcia")
+job_title <- c("Professional", "Programmer", "Management", "Clerical", "Developer", "Programmer", "Management", "Clerical", "Developer", "Programmer")
+employee <- data.frame(id, name, job_title)
+
+print(employee)
+
+# 1. separate()
+separate(employee, name, into = c("first_name", "last_name"), sep=' ')
+
+# 2. unite()
+first_name <- c("John", "Rob", "Rachel", "Christy", "Johnson", "Candace", "Carlson", "Pansy", "Darius", "Claudia")
+
+last_name <- c("Mendes", "Stewart", "Abrahamson", "Hickman", "Harper", "Miller", "Landy", "Jordan", "Berry", "Garcia")
+
+job_title <- c("Professional", "Programmer", "Management", "Clerical", "Developer", "Programmer", "Management", "Clerical", "Developer", "Programmer")
+
+employee <- data.frame(id, first_name, last_name, job_title)
+
+print(employee)
+unite(employee, 'Name', first_name, last_name, sep=' ')
+
+# 3. mutate()
+# Membuat variabel baru dalam dataframe menggunakan mutate, menambahkan kolom dengan perhitungan kalkulasi
+
+view(penguins)
+
+penguins %>%
+  mutate(body_mass_kg = body_mass_g/1000, flipper_length_m = flipper_length_mm/1000)  %>% drop_na()
