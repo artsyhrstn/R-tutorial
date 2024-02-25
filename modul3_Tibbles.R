c32c0d590fcb75e4a92f29f4517c50c87204eeaa
Hisbi Asyihristani <hisbiasyhrstn@gmail.com>
2024-02-25 17:42:08 +0700

    Update

---
+++ modul3_Tibbles.R
@@ -0,0 +1,8 @@
+# More About Tibbles
+# Tibbles adalah inti dari struktur data tidyverse dan digunakan untuk memfasilitasi tampilan dan analisis informasi dalam format yang rapi. Tibbles adalah bentuk baru dari dataframe di mana bingkai data adalah struktur data yang paling umum digunakan untuk menyimpan kumpulan data di R
+
+library(tidyverse)
+data(diamonds)
+
+view(diamonds)
+as_tibble(diamonds)
