6ea953fddbf10262b4d08b02fb139fb66e89ea24
hisbi asyihristani <hisbiasyhrstn@gmail.com>
2024-10-12 22:38:17 +0700

    add spasial etx

--- Coursera/install_packages.r
+++
@@ -1,5 +0,0 @@
-# library membuaka file .dta ataupun file berformat tabel
-install.packages("Rcpp")
-
-# pengolahan data import dan export data, membuat visualisasi data, dan pemodelan data.
-install.packages("Rcpp")
